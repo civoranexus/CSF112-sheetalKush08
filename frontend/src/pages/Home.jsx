5525710ebe0d124277313d1d67096e6f5582b599
sheetalKushwaha <kushwahasheetal2105@gmail.com>
2026-01-23 18:58:40 +0530

    Added footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/common/Navbar'
 import Hero from '../components/common/Hero'
+import Footer from '../components/common/Footer'
 
 const Home = () => {
   return (
     <>
     <Navbar />
     <Hero />
+    <Footer />
       
     </>
   )
